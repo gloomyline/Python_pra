91fcb6ed33e44b487fbc54d95a7b2facacb1a272
gloomy <1211071880@qq.com>
2016-10-20 21:05:36 +0800

    delete .gitignore & complete how_to_find_public_keys.py

--- advanced_python_practices/how_to_find_public_keys.py
+++ advanced_python_practices/how_to_find_public_keys.py
@@ -13,12 +13,22 @@ dicts_l = []
 for _ in xrange(3):
     d = {x: randint(1, 4) for x in sample('abcdefg', randint(3, 7))}
     dicts_l.append(d)
+print dicts_l
 
+print 'use iter to find public key'
 res = []
 for k in dicts_l[0].keys():
     if k in dicts_l[1] and k in dicts_l[2]:
         res.append(k)
 print res
+print '----------------'
 
+print 'use mixed set'
+print dicts_l[0].viewkeys() & dicts_l[1].viewkeys() & dicts_l[2].viewkeys()
+print '----------------'
 
-print 'use iter to find public key'
+print 'use reduce'
+m = map(dict.viewkeys, dicts_l)
+print m
+res1 = reduce(lambda x, y: x & y, m)
+print res1
