3dc408a42c34bba521e63fb89a6d70340226ccb8
gloomyline <1211071880@qq.com>
2016-10-20 17:59:43 +0800

    init find_public_keys_in_dict.py

---
+++ advanced_python_practices/how_to_find_public_keys.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on 2016/10/20
+@fn:
+@author: Alan Wang
+"""
+
+
+from random import randint, sample
+
+print 'generate random dicts'
+dicts_l = []
+for _ in xrange(3):
+    d = {x: randint(1, 4) for x in sample('abcdefg', randint(3, 7))}
+    dicts_l.append(d)
+
+res = []
+for k in dicts_l[0].keys():
+    if k in dicts_l[1] and k in dicts_l[2]:
+        res.append(k)
+print res
+
+
+print 'use iter to find public key'
