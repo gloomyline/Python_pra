1598334368b3cfdfe4ac68a008cd6e1fc1840241
gloomyline <1211071880@qq.com>
2016-10-20 15:10:05 +0800

    add practice of common method  to get statistics

--- advanced_python_practices/get_statistics_from_sequences.py
+++ advanced_python_practices/get_statistics_from_sequences.py
@@ -6,7 +6,9 @@
 @Software: PyCharm Community Edition
 """
 
+import re
 from random import randint
+from collections import Counter
 
 data = [randint(0, 20) for _ in xrange(30)]
 print '生成30个随机的0到20之间的数的列表', data
@@ -18,7 +20,23 @@ for x in data:
     c[x] += 1
 
 # after get statistics
-print '统计后的字典', c
+print '迭代字典统计后的字典', c
 
-# sort c
-# sorted(c, cmp=lambda c.x, c.y: )
+# sorted c by v
+c_value_list = []
+for k, v in c.iteritems():
+    c_value_list.append(v)
+print sorted(c_value_list, reverse=True)
+
+print '----------'
+
+c1 = Counter(data)
+print '使用Counter类统计后的字典', c1
+
+print '出现频率最高的3项', c1.most_common(3)
+
+txt = open('get_statistics_from_sequences.txt').read()
+str_l = re.split('\W+', txt)
+c2 = Counter(str_l)
+print c2
+print c2.most_common(10)
